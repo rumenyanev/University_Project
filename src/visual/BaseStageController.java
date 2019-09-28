d370472b1e1bc37c9e6c53b64e40baeaf442b8c7
zspace <zstefchev@gmail.com>
2019-09-28 16:16:24 +0300

    vp changes

--- src/visual/BaseStageController.java
+++ src/visual/BaseStageController.java
@@ -1,5 +1,5 @@
 package visual;
-
+//Да не се стартира от Main на visual пакета!!! Има грешки за оправяне! :)
 import core.Manipulator;
 import javafx.fxml.FXML;
 
@@ -16,63 +16,21 @@ public class BaseStageController {
     @FXML // fx:id="firstNameStudent"
     private TextField firstNameStudent; // Value injected by FXMLLoader
 
-    @FXML // fx:id="lastNameStudent"
-    private TextField lastNameStudent; // Value injected by FXMLLoader
-
-    @FXML // fx:id="emailStudent"
-    private TextField emailStudent; // Value injected by FXMLLoader
-
-    @FXML // fx:id="ageStudent"
-    private TextField ageStudent; // Value injected by FXMLLoader
-
-    @FXML // fx:id="departmentStudent"
-    private TextField departmentStudent; // Value injected by FXMLLoader
-
-    @FXML // fx:id="testButton"
-    private Button testButton; // Value injected by FXMLLoader
-
-    @FXML // fx:id="exitButon"
-    private Button exitButon; // Value injected by FXMLLoader
-
-    @FXML // fx:id="resetButton"
-    private Button resetButton; // Value injected by FXMLLoader
-
-    @FXML // fx:id="saveButton"
-    private Button saveButton; // Value injected by FXMLLoader
-
-    @FXML
-    public void exitButtonClicked(ActionEvent event) {
-
+    public void sendInformationButtonClicked() {
+        TextField firstNameStudent = this.firstNameStudent;
+        //System.out.println(this.firstNameStudent.get);
     }
 
-    @FXML
-    public void sendInformationButtonClicked(ActionEvent event) {
-        System.out.println("GO GO GO");
-    }
-
-    @FXML
-    public void testButtonClicked(ActionEvent event) {
-
-    }
-
-    /*@FXML
-    public void sendInformationButtonClicked() throws IOException {
-        //sendInformation.set;
-    }*/
-
-    @FXML
     public void testButtonClicked() throws IOException {
         System.out.println("Test start of program...");
         Manipulator m = new Manipulator();
         m.run();
     }
 
-    @FXML
     public void exitButtonClicked() {
         System.exit(0);
     }
 
-    @FXML
     public void facebookLinkClicked() throws IOException, URISyntaxException {
         Desktop d = Desktop.getDesktop();
         d.browse(new URI("http://facebook.com"));
